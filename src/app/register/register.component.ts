37e183d55570eb3804f860efaa369ba403217a43
ToDy95 <todyimmortal@gmail.com>
2021-09-30 02:33:31 +0300

    si required la register pe email si ambele pass, dumnezo cu mila ma culc

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -6,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+reactiveForm;
+  constructor(private formBuilder: FormBuilder) {
+    this.reactiveForm = this.formBuilder.group({
+      email : new FormControl(null, [Validators.required]),
+      password : new FormControl(null, [Validators.required]),
+      passwordM : new FormControl(null, [Validators.required])
+    })
+   }
 
-  constructor() { }
-
+  get f(){return this.reactiveForm.controls}
+submitted;
+  onSubmit(){
+    this.submitted = true;
+    if(this.reactiveForm.invalid){
+      return;
+    }
+  }
   ngOnInit(): void {
   }
 
